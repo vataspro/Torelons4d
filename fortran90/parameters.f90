9c2c88c2cb93455488adb2adb18b8119a5358f68
Kieran Twaites <kieran.twaites@physics.ox.ac.uk>
2026-09-21 14:52:58 +0100

    Added initialise_parameters function to read parameters from parameter file. Only for NCOL, LX1, LX2, LX3, LX4 so far

--- fortran90/parameters.f90
+++ fortran90/parameters.f90
@@ -12,4 +12,26 @@ module parameters
     integer :: MAX_DELTA_T ! Maximum time extent of correlation functions, renamed from MAXDTLS
 
     !! TODO: implement all other global parameters
+
+    contains
+
+    ! Read all parameters from file and set dependant parameters
+    subroutine initialise_parameters(parameter_filename)
+        implicit none
+        character(len=*) :: parameter_filename
+
+        ! Define variables to read in from parameter file
+        namelist /params/ NCOL, LX1, LX2, LX3, LX4
+
+        ! Read parameters from parameter file
+        open(10, file=trim(parameter_filename))
+        read(10, params)
+        close(10)
+
+        ! Set dependant parameters
+        SLICE_VOLUME = LX1 * LX2 * LX3
+        LATTICE_VOLUME = SLICE_VOLUME * LX4
+        NCOL2 = NCOL * NCOL
+        MAX_DELTA_T = LX4 / 2
+    end subroutine
 end module
