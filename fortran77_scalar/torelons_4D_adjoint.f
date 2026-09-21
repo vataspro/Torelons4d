de7b57dd8f5dc2a8ab799a88cd6fc83c35799323
Alexis Provatas <vataspro@gmail.com>
2026-09-21 15:39:12 +0300

    close output files in subroutine TODISK2

--- fortran77_scalar/torelons_4D_adjoint.f
+++ fortran77_scalar/torelons_4D_adjoint.f
@@ -886,6 +886,27 @@ C
 C****************************************************
          ENDDO
       ENDDO
+C *** CLOSE OUTPUT FILES ***
+      CLOSE(17)
+      CLOSE(18)
+      CLOSE(19)
+      CLOSE(20)
+      CLOSE(21)
+      CLOSE(22)
+      CLOSE(23)
+      CLOSE(24)
+      CLOSE(25)
+      CLOSE(26)
+      CLOSE(27)
+      CLOSE(28)
+      CLOSE(29)
+      CLOSE(30)
+      CLOSE(31)
+      CLOSE(32)
+      CLOSE(33)
+      CLOSE(34)
+      CLOSE(35)
+      CLOSE(36)
 C*******************************************************************
       RETURN
       END
