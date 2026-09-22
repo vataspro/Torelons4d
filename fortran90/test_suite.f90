d679c2ded397231957d59471ca7946ed452b1f0c
Kieran Twaites <kieran.twaites@physics.ox.ac.uk>
2026-09-22 11:49:51 +0100

    Fixed directory to testing configuration in test_suite.f90

--- fortran90/test_suite.f90
+++ fortran90/test_suite.f90
@@ -93,9 +93,9 @@ program test_suite
         complex(real32) :: gauge_field_correct(5), gauge_field_check(5)
 
         ! Set directory path
-        homepath=trim('/home/dp208/dp208/dc-athe1/AXIONS/NF2/b2.3/')
-        conf_directory=trim('m-1.0/26x26x26x52/confs/')
-        conf_filename=trim('run1_52x26x26x26nc2rADJnf2b2.300000m1.000000n34780')
+        homepath=trim('/mnt/lustre/tursafs1/home/dp208/dp208/shared/runs/axion/')
+        conf_directory=trim('nf2/beta2.3/m-1.0/26x26x26x52/cnfg/')
+        conf_filename=trim('run1_52x26x26x26nc2rADJnf2b2.300000m1.000000n134780')
         directory=trim(homepath) // trim(conf_directory) // trim(conf_filename)
 
         ! Load gauge field into memory
