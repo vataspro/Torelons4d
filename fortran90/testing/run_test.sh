0a728bf9711b8088b71350a8b60d2ba99948b75b
ktwaites <kieran.twaites@physics.ox.ac.uk>
2026-09-23 12:57:23 +0100

    Potential fix for run_test.sh

--- fortran90/testing/run_test.sh
+++ fortran90/testing/run_test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-gfortran -c parameters.f90 lattice.f90 read_field_config.f90 -I../
+gfortran -c ../parameters.f90 -I/../
+gfortran -c ../lattice.f90 -I/../
+gfortran -c ../read_field_config.f90 -I/../
 gfortran -o test_suite test_suite.f90 parameters.o lattice.o read_field_config.o
 ./test_suite
