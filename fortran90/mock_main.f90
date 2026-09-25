5617fb8949d3446ed754174f3f4501a1b5fcd1cd
Alexis Provatas <vataspro@gmail.com>
2026-09-25 16:18:27 +0300

    add mock main to check vs original code

--- fortran90/mock_main.f90
+++ fortran90/mock_main.f90
@@ -12,8 +12,9 @@ program mock_main
     ! Happy testing!
     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-    complex(real64), allocatable :: gauge_field(:,:,:,:)
-    integer(int32) :: mu, site_idx
+    complex(real64), allocatable :: gauge_field(:,:,:,:), blok_timeslice(:,:,:,:,:), A11(:,:)
+    integer(int32) :: mu, site_idx, blevel, site_in, site_out
+
 
     ! Read parameters from input file
     call initialise_parameters("parameter_file.txt")
@@ -21,16 +22,33 @@ program mock_main
     ! Setup lattice pointers
     call setup_lattice()
 
-    ! Allocate gauge field so that it only needs to be loaded once
+    ! Allocate arrays
     allocate(gauge_field(NCOL, NCOL, LATTICE_VOLUME, 4))
+    allocate(A11(NCOL, NCOL))
+
+    A11 = get_I(2)
+    call print_matrix(A11)
+
+    ! Load gauge field
     call read_gauge_field("/Users/alexi/Work/phd/torelons/cnfg/run1_52x26x26x26nc2rADJnf2b2.300000m1.000000n134780", gauge_field)
 
+    ! Get the blocked first time slice
+    blok_timeslice = get_blocked_gauge_field(gauge_field(:, :, 1:SLICE_VOLUME, 1:3))
+
+    ! get square pulses in `squares_up` and `squares_down`
+    call get_square_pulses(blok_timeslice, 1)
+    call get_plaquettes(blok_timeslice, 1)
+
+    call loop_1(1, site_out, A11, 2)
+    call print_matrix(A11)
     
-    do mu=1,4
-        do site_idx=1,LATTICE_VOLUME
-            call print_matrix(gauge_field(:, :, site_idx, mu))
-        enddo
-    enddo
+    ! do blevel=1,MAX_BLOCKING_LEVEL
+    !     do mu=1,2
+    !         do site_idx=1,SLICE_VOLUME
+    !             call print_matrix(squares_down(:, :, site_idx, mu, blevel))
+    !         enddo
+    !     enddo
+    ! enddo
 
     contains
 
