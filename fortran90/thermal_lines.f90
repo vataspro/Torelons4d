b51e9778a4260b66f969cf23fa32dd49b9ea81f1
Gianmarco Simonetti <gianmarco.simonetti6@gmail.com>
2026-09-25 14:41:42 +0300

    Take thermal lines from dev-alexis

--- fortran90/thermal_lines.f90
+++ fortran90/thermal_lines.f90
@@ -3,31 +3,103 @@ module thermal_lines
     use lattice
     implicit none
 
-    complex(real64), allocatable :: squares_up(:,:,:,:,:), squares_down(:,:,:,:,:)
-    integer :: direction_index(3)
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    !
+    ! --------------------------------------- THERML1 UPDATE FUNCTIONS ---------------------------------------
+    !
+    ! This module contains functions to calculate all square pulses and plaquettes on the lattice, used for 
+    ! creating Torelon loops, with torelon flux pointing in a given direction.
+    ! 
+    ! To make use of these functions:
+    ! - Nothing needs to be called in the parent code before the first call of THERML1
+    !
+    ! - Before the momentum sum (loop over sites) is initiated, the get_square_pulses and get_plaquettes should
+    !   be called. These take as input:
+    !    * The fully-blocked gauge field on a single time slice
+    !    * The direction of the Torelon flux (!to match with the old code, this should be set to 1!)
+    !   These functions store all square pulses and plaquettes in a memory-efficient way in the global
+    !   variables squares_up, squares_down and plaquettes. After these function calls, the functions
+    !   square_pulse and plaquette become available.
+    !
+    ! - The function square_pulse returns the square pulse in the requested direction, from the requested site.
+    !   It takes as input:
+    !    * The index of the site at which flux enters the square pulse
+    !    * The index of the square pulse
+    !    * The blocking level of the square pulse
+    !   This maps to variables defined in the old code in the following way:
+    !    * SQUY1 -> square_pulse(<site>, +2, <blocking_level>)
+    !    * SQUZ1 -> square_pulse(<site>, +3, <blocking_level>)
+    !    * SQDY1 -> square_pulse(<site>, -2, <blocking_level>)
+    !    * SQDZ1 -> square_pulse(<site>, -3, <blocking_level>)
+    !    * SQUY2 -> square_pulse(move(<site>, +1, <blocking_level>), +2, <blocking_level>)
+    !    * SQUZ2 -> square_pulse(move(<site>, +1, <blocking_level>), +3, <blocking_level>)
+    !    * SQDY2 -> square_pulse(move(<site>, +1, <blocking_level>), -2, <blocking_level>)
+    !    * SQDZ2 -> square_pulse(move(<site>, +1, <blocking_level>), -3, <blocking_level>)
+    !
+    ! - The function plaquette returns the plaquette in the plane orthogonal to the flux tube with the
+    !   requested index (detailed below), from the requested site. It takes as input:
+    !    * The index of the site at which flux enters the plaquette
+    !    * The index of the plaquette
+    !    * The blocking level of the plaquette
+    !   This function uses the convention of anti-clockwise flux around the plaquette, consistent with a
+    !   'right-hand rule' in the direction of the Torelon flux. Dentoing mu and nu to be the directions in the
+    !   plane perpendicular to the flux direction, they are defined cyclically:
+    !   flux direction|____mu|____nu|
+    !                1|     2|     3|
+    !                2|     3|     1|
+    !                3|     1|     2|
+    !   The rotations of plaquettes are then indexed in the following way:
+    !   ____(mu, nu)|____index|
+    !         (+, +)|        1|
+    !         (-, +)|        2|
+    !         (-, -)|        3|
+    !         (+, -)|        4|
+    !   Inputting the negative of these indices gives the equivilant (mu, nu) plaquette with opposite
+    !   circulation.
+    !   This maps to variables defined in the old code in the following way:
+    !    * PLQ1  -> plaquette(<site>, +1, <blocking_level>)
+    !    * PLQ2  -> plaquette(<site>, +2, <blocking_level>) 
+    !    * PLQ3  -> plaquette(<site>, +3, <blocking_level>) 
+    !    * PLQ4  -> plaquette(<site>, +4, <blocking_level>) 
+    !    * PLQ5  -> plaquette(<site>, -1, <blocking_level>)
+    !    * PLQ6  -> plaquette(<site>, -2, <blocking_level>) 
+    !    * PLQ7  -> plaquette(<site>, -3, <blocking_level>) 
+    !    * PLQ8  -> plaquette(<site>, -4, <blocking_level>)
+    !    * DPLQ1 -> plaquette(move(<site>, +1, <blocking_level>), +1, <blocking_level>)
+    !    * DPLQ2 -> plaquette(move(<site>, +1, <blocking_level>), +2, <blocking_level>) 
+    !    * DPLQ3 -> plaquette(move(<site>, +1, <blocking_level>), +3, <blocking_level>) 
+    !    * DPLQ4 -> plaquette(move(<site>, +1, <blocking_level>), +4, <blocking_level>) 
+    !    * DPLQ5 -> plaquette(move(<site>, +1, <blocking_level>), -1, <blocking_level>)
+    !    * DPLQ6 -> plaquette(move(<site>, +1, <blocking_level>), -2, <blocking_level>) 
+    !    * DPLQ7 -> plaquette(move(<site>, +1, <blocking_level>), -3, <blocking_level>) 
+    !    * DPLQ8 -> plaquette(move(<site>, +1, <blocking_level>), -4, <blocking_level>)
+    !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    complex(real64), allocatable :: squares_up(:,:,:,:,:), squares_down(:,:,:,:,:), plaquettes(:,:,:,:)
+    integer :: squares_direction_index(3), plaquette_direction_index(3)
 
     contains
 
     ! Calculate square pulse from given site in given positive direction
-    function square_pulse_up(gauge_field, blocking_level, site, flux_direction, pulse_direction)
+    function calculate_square_pulse_up(gauge_field, blocking_level, site, flux_direction, pulse_direction)
         implicit none
         complex(real64), intent(in) :: gauge_field(NCOL, NCOL, SLICE_VOLUME, 3)
         integer, intent(in) :: blocking_level, site, flux_direction, pulse_direction
-        complex(real64) :: square_pulse_up(NCOL, NCOL)
+        complex(real64) :: calculate_square_pulse_up(NCOL, NCOL)
 
         integer :: site_plus_flux, site_plus_pulse
 
         ! Check flux and pulse directions are compatibile
         if (abs(flux_direction) == abs(pulse_direction)) error stop &
-        "Flux direction cannot equal pulse direction in square_pulse_up"
+        "Flux direction cannot equal pulse direction in calculate_square_pulse_up"
 
         ! Check flux points in positive direction
         if (flux_direction <= 0) error stop &
-        "Flux direction must be passed as positive in square_pulse_up"
+        "Flux direction must be passed as positive in calculate_square_pulse_up"
 
         ! Check deformation points in positive direction
         if (pulse_direction <= 0) error stop &
-        "Pulse direction must be passed as positive in square_pulse_up"
+        "Pulse direction must be passed as positive in calculate_square_pulse_up"
 
         ! Check both directions point in spatial directions
         if ((flux_direction > 3).or.(pulse_direction > 3)) error stop &
@@ -40,32 +112,32 @@ module thermal_lines
         site_plus_pulse = move(site, pulse_direction, blocking_level)
 
         ! Get square pulse
-        square_pulse_up = matmul(matmul(&
+        calculate_square_pulse_up = matmul(matmul(&
             gauge_field(:,:,site,pulse_direction), &
             gauge_field(:,:,site_plus_pulse,flux_direction)), &
             herm(gauge_field(:,:,site_plus_flux,pulse_direction)))
     end function
 
     ! Calculate square pulse from given site in given negative direction
-    function square_pulse_down(gauge_field, blocking_level, site, flux_direction, pulse_direction)
+    function calculate_square_pulse_down(gauge_field, blocking_level, site, flux_direction, pulse_direction)
         implicit none
         complex(real64), intent(in) :: gauge_field(NCOL, NCOL, SLICE_VOLUME, 3)
         integer, intent(in) :: blocking_level, site, flux_direction, pulse_direction
-        complex(real64) :: square_pulse_down(NCOL, NCOL)
+        complex(real64) :: calculate_square_pulse_down(NCOL, NCOL)
 
         integer :: site_minus_pulse, corner_site
 
         ! Check flux and pulse directions are compatibile
         if (abs(flux_direction) == abs(pulse_direction)) error stop &
-        "Flux direction cannot equal pulse direction in square_pulse_down"
+        "Flux direction cannot equal pulse direction in calculate_square_pulse_down"
 
         ! Check flux points in positive direction
         if (flux_direction <= 0) error stop &
-        "Flux direction must be passed as positive in square_pulse_down"
+        "Flux direction must be passed as positive in calculate_square_pulse_down"
 
         ! Check deformation points in positive direction
         if (pulse_direction <= 0) error stop &
-        "Pulse direction must be passed as positive in square_pulse_down"
+        "Pulse direction must be passed as positive in calculate_square_pulse_down"
 
         ! Check both directions point in spatial directions
         if ((flux_direction > 3).or.(pulse_direction > 3)) error stop &
@@ -78,10 +150,39 @@ module thermal_lines
         corner_site = move(site_minus_pulse, flux_direction, blocking_level)
 
         ! Get square pulse
-        square_pulse_down = matmul(matmul(&
+        calculate_square_pulse_down = matmul(matmul(&
             herm(gauge_field(:,:,site_minus_pulse,pulse_direction)), &
             gauge_field(:,:,site_minus_pulse,flux_direction)), &
             gauge_field(:,:,corner_site,pulse_direction))
+    end function
+
+    ! Calculate plaquette from given site in (mu, nu) = (+, +) direction
+    function calculate_plaquette(gauge_field, blocking_level, site, flux_direction)
+        implicit none
+        complex(real64), intent(in) :: gauge_field(NCOL, NCOL, SLICE_VOLUME, 3)
+        integer, intent(in) :: blocking_level, site, flux_direction
+        complex(real64) :: calculate_plaquette(NCOL, NCOL)
+
+        integer :: mu, nu, site_plus_mu, site_plus_nu
+
+        ! Check flux direction is compatible with directions stored in plaquette_direction_index
+        if (flux_direction /= plaquette_direction_index(1)) error stop &
+        "flux_direction incompatible with plaquette_direction_index in calculate_plaquette"
+
+        ! Set mu and nu from plaquette_direction_index
+        mu = plaquette_direction_index(2)
+        nu = plaquette_direction_index(3)
+
+        ! Get sites in directions +mu and +nu from site
+        site_plus_mu = move(site, mu, blocking_level)
+        site_plus_nu = move(site, nu, blocking_level)
+
+        ! Calculate plaquette in positive (mu, nu) direction
+        calculate_plaquette = matmul(matmul(matmul(&
+                              gauge_field(:, :, site, mu), &
+                              gauge_field(:, :, site_plus_mu, nu)), &
+                              herm(gauge_field(:, :, site_plus_nu, mu))), &
+                              herm(gauge_field(:, :, site, nu)))
     end function
 
     ! Calculate all square pulses in orthogonal directions over all blocking levels on the lattice
@@ -100,13 +201,13 @@ module thermal_lines
 
         ! Iterate over lattice and calculate all square pulses in directions orthogonal to the flux direction
         dir_count = 1
-        direction_index = 0
+        squares_direction_index = 0
         do pulse_direction = 1, 3
             ! Skip direction parallel to flux direction
             if (pulse_direction == flux_direction) cycle
 
             ! Set direction index. This is a small array to map between pulse_direction and the position of the relevant entry in the 4th dimension of the squares_up/down arrays
-            direction_index(pulse_direction) = dir_count
+            squares_direction_index(pulse_direction) = dir_count
 
             ! Iterate over all blocking levels
             do blocking_level = 1, MAX_BLOCKING_LEVEL
@@ -114,16 +215,47 @@ module thermal_lines
                 do site = 1, SLICE_VOLUME
                     ! Calculate square pulses
                     squares_up(:, :, site, dir_count, blocking_level) &
-                    = square_pulse_up(gauge_field_blocked(:, :, :, :, blocking_level), &
-                                      blocking_level, site, flux_direction, pulse_direction)
+                    = calculate_square_pulse_up(gauge_field_blocked(:, :, :, :, blocking_level), &
+                                                blocking_level, site, flux_direction, pulse_direction)
                     squares_down(:, :, site, dir_count, blocking_level) &
-                    = square_pulse_down(gauge_field_blocked(:, :, :, :, blocking_level), &
-                                        blocking_level, site, flux_direction, pulse_direction)
+                    = calculate_square_pulse_down(gauge_field_blocked(:, :, :, :, blocking_level), &
+                                                  blocking_level, site, flux_direction, pulse_direction)
                 enddo
             enddo
 
             ! Iterate dir_count
             dir_count = dir_count + 1
+        enddo
+    end subroutine
+
+    ! Calculate all plaquettes in plane orthogonal to the flux direction over all blocking levels on the lattice
+    subroutine get_plaquettes(gauge_field_blocked, flux_direction)
+        implicit none
+        complex(real64), intent(in) :: gauge_field_blocked(NCOL, NCOL, SLICE_VOLUME, 3, MAX_BLOCKING_LEVEL)
+        integer, intent(in) :: flux_direction
+
+        integer :: site, blocking_level
+
+        ! Check if plaquette container is allocated. If not, allocate it.
+        if (.not.allocated(plaquettes)) allocate(plaquettes(NCOL, NCOL, SLICE_VOLUME, MAX_BLOCKING_LEVEL))
+
+        ! Get orthogonal directions to the flux direction in order of cyclic permutations
+        ! flux_direction|____mu|____nu|
+        !              1|     2|     3|
+        !              2|     3|     1|
+        !              3|     1|     2|
+        plaquette_direction_index(1) = flux_direction
+        plaquette_direction_index(2) = mod(flux_direction, 3) + 1 ! mu
+        plaquette_direction_index(3) = 6 - plaquette_direction_index(1) - plaquette_direction_index(2) ! nu
+
+        ! Iterate over lattice and calculate all plaquettes in plane orthogonal to the flux direction
+        do blocking_level = 1, MAX_BLOCKING_LEVEL
+            do site = 1, SLICE_VOLUME
+                ! Calculate plaquette
+                plaquettes(:, :, site, blocking_level) &
+                = calculate_plaquette(gauge_field_blocked(:, :, :, :, blocking_level), &
+                                      blocking_level, site, flux_direction)
+            enddo
         enddo
     end subroutine
 
@@ -134,7 +266,7 @@ module thermal_lines
         complex(real64) :: square_pulse(NCOL, NCOL)
 
         ! Check pulse direction is not parallel to flux direction
-        if (direction_index(abs(pulse_direction)) == 0) error stop &
+        if (squares_direction_index(abs(pulse_direction)) == 0) error stop &
         "Pulse direction must not be parallel to flux direction in square_pulse"
 
         ! Check pulse direction is a valid direction
@@ -143,12 +275,71 @@ module thermal_lines
 
         ! Return square pulse in given direction by looking up value in squares_up or squares_down
         if (pulse_direction > 0) then
-            square_pulse = squares_up(:, :, site, direction_index(pulse_direction), blocking_level)
+            square_pulse = &
+            squares_up(:, :, site, squares_direction_index(pulse_direction), blocking_level)
         else
-            square_pulse = squares_down(:, :, site, direction_index(abs(pulse_direction)), blocking_level)
+            square_pulse = &
+            squares_down(:, :, site, squares_direction_index(abs(pulse_direction)), blocking_level)
         endif
     end function
 
+    ! Return plaquette from given site, with given index
+    function plaquette(site, plaquette_index, blocking_level)
+        implicit none
+        integer, intent(in) :: site, plaquette_index, blocking_level
+        complex(real64) :: plaquette(NCOL, NCOL)
+
+        integer :: mu, nu
+
+        ! Set mu and nu from plaquette_direction_index
+        mu = plaquette_direction_index(2)
+        nu = plaquette_direction_index(3)
+
+        ! Get correct plaquette based on plaquette_index
+        select case(plaquette_index)
+        case(1)
+            ! (mu, nu) = (+, +)
+            plaquette = plaquettes(:, :, site, blocking_level)
+        case(2)
+            ! (mu, nu) = (-, +), equivalent to plaquette at (site - mu)
+            plaquette = plaquettes(:, :, &
+                                   move(site, -mu, blocking_level), &
+                                   blocking_level)
+        case(3)
+            ! (mu, nu) = (-, -), equivalent to plaquette at (site - mu - nu)
+            plaquette = plaquettes(:, :, &
+                                   move(move(site, -mu, blocking_level), -nu, blocking_level), &
+                                   blocking_level)
+        case(4)
+            ! (mu, nu) = (+, -), equivalent to plaquette at (site - nu)
+            plaquette = plaquettes(:, :, &
+                                   move(site, -nu, blocking_level), &
+                                   blocking_level)
+        case(-1)
+            ! PLQ5 = herm(PLQ2)
+            plaquette = herm(plaquettes(:, :, &
+                                        move(site, -mu, blocking_level), &
+                                        blocking_level))
+        case(-2)
+            ! PLQ6 = herm(PLQ1)
+            plaquette = herm(plaquettes(:, :, site, blocking_level))
+        case(-3)
+            ! PLQ7 = herm(PLQ4)
+            plaquette = herm(plaquettes(:, :, &
+                                        move(site, -nu, blocking_level), &
+                                        blocking_level))
+        case(-4)
+            ! PLQ8 = herm(PLQ3)
+            plaquette = herm(plaquettes(:, :, &
+                                        move(move(site, -mu, blocking_level), -nu, blocking_level), &
+                                        blocking_level))
+        case default
+            ! plaquette_index is not a valid index
+            error stop "Plaquette index must be +-1, +-2, +-3 or +-4"
+        end select
+    end function
+
+    ! Loop 1
     !##############################################
     !           SQUARE PULSES
     !        __
@@ -156,7 +347,6 @@ module thermal_lines
     !                         |__|
     !
     !##############################################
-    ! Loop 1: Up square pulse Y
     subroutine loop_1(in_site, out_site, A11, blocking_level)
         implicit none
         integer, intent(in) :: in_site, blocking_level
